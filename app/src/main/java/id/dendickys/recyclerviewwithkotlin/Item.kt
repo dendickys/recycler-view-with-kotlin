e90fe9489106b7e4d9249e8c620c230086dd152a
dendickys <febri.unquad2014@gmail.com>
2020-09-27 12:51:30 +0700

    add kotlin android extensions

--- app/src/main/java/id/dendickys/recyclerviewwithkotlin/Item.kt
+++ app/src/main/java/id/dendickys/recyclerviewwithkotlin/Item.kt
@@ -1,3 +1,7 @@
 package id.dendickys.recyclerviewwithkotlin
 
-data class Item (val name: String?, val image: Int?)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Item (val name: String?, val image: Int?) : Parcelable
--- app/src/main/java/id/dendickys/recyclerviewwithkotlin/RecyclerViewAdapter.kt
+++ app/src/main/java/id/dendickys/recyclerviewwithkotlin/RecyclerViewAdapter.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list.view.*
 
-class RecyclerViewAdapter(private val context: Context, private val items: List<Item>, private val listener: (Item) -> Unit) :
+class RecyclerViewAdapter(
+    private val context: Context,
+    private val items: List<Item>,
+    private val listener: (Item) -> Unit
+) :
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
@@ -21,14 +26,13 @@ class RecyclerViewAdapter(private val context: Context, private val items: List<
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val name = view.findViewById<TextView>(R.id.name)
-        private val image = view.findViewById<ImageView>(R.id.image)
+    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer {
 
         fun bindItem(items: Item, listener: (Item) -> Unit) {
-            name.text = items.name
+            itemView.name.text = items.name
             items.image?.let { Picasso.get().load(it).fit().into(image) }
-            itemView.setOnClickListener {
+            containerView.setOnClickListener {
                 listener(items)
             }
         }
